647f59e4a739d81912524f18ed12ec0bc94013b5
Aaron D <aaron@dutschmann-online.de>
2025-02-22 17:58:50 +0100

    feature(frontnd): add initial input

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+  import userInput from './components/userInput.vue'
 </script>
 
 <template>
-  <h1>MoM Team 13</h1>
+  <header>
+    <h1>MoM Team 13</h1>
+  </header>
+  <main>
+    <userInput />
+  </main>
 </template>
 
 <style scoped>
+:host {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
 </style>
---
+++ frontend/src/components/userInput.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <input v-model="input" placeholder="Enter your name" />
+        <button @click="submit">Submit</button>
+    </div>
+</template>
+
+<style scoped>
+
+
+</style>
