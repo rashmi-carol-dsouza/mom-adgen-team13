61e3ffedc305d577ff1c6a5e5cace12c1ba1c8b7
Larry D Almeida <hello@larrydalmeida.com>
2025-02-23 11:32:22 +0100

    Use lambda layers

--- bff-api/deploy/modules/lambda/main.tf
+++ bff-api/deploy/modules/lambda/main.tf
@@ -13,6 +13,27 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+resource "aws_iam_policy" "lambda_layer_access" {
+  name        = "${var.function_name}_layer_access"
+  description = "Allows Lambda function to access public layers"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "lambda:GetLayerVersion"
+        Resource = "arn:aws:lambda:eu-central-1:898466741470:layer:psycopg2-py38:1"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_layer_access_attachment" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_layer_access.arn
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
@@ -31,4 +52,8 @@ resource "aws_lambda_function" "this" {
   role             = aws_iam_role.lambda_exec.arn
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  layers = [
+    "arn:aws:lambda:eu-central-1:898466741470:layer:psycopg2-py38:1"
+  ]
 }
