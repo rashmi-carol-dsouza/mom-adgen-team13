f812cfecea488a4f4320c13117f6c6144993724f
Larry D Almeida <hello@larrydalmeida.com>
2025-02-23 01:47:22 +0100

    Fix IAM role

--- bff-api/lambda/lambda_function.py
+++ bff-api/lambda/lambda_function.py
@@ -1,3 +1,14 @@
 # pylint: disable=unused-argument
+import base64
+
+
 def lambda_handler(event, context):
-    return {"statusCode": 200, "body": "Hello, world!"}
+    with open("example-advert.mp3", "rb") as f:
+        mp3_data = f.read()
+    encoded_data = base64.b64encode(mp3_data).decode("utf-8")
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "audio/mpeg"},
+        "isBase64Encoded": True,
+        "body": encoded_data,
+    }
