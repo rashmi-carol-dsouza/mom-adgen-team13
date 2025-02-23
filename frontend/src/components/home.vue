bbcd98f1aa7fa7d428f58a330b3e3fbaa52fe6d3
Aaron D <aaron@dutschmann-online.de>
2025-02-23 15:33:36 +0100

    feat(frontend): remove manual inputs

--- frontend/src/components/home.vue
+++ frontend/src/components/home.vue
@@ -1,27 +1,9 @@
 <script setup>
-import { ref } from 'vue';
-import userInput from './userInput.vue';
-import player from './player.vue';
 import Login from './login.vue';
-
-const status = ref('form');
-let adFromAPI = ref();
-
-const handleStatusChange = (newStatus) => {
-  status.value = newStatus;
-};
-
-const handleDataLoaded = (fileUrl) => {
-  adFromAPI.value = fileUrl;
-  status.value = 'finished';
-};
 </script>
 
 <template>
     <Login />
-    <userInput @status-change="handleStatusChange" @data-loaded="handleDataLoaded" />
-    <v-progress-circular v-if="status === 'loading'" indeterminate color="primary"></v-progress-circular>
-    <player v-if="status === 'finished'" />
 </template>
 
 <style scoped>
