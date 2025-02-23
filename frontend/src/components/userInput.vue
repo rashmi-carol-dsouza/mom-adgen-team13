f3ba7c0592afaed1d7e3d88b7d801cda9dbc227c
Aaron D <aaron@dutschmann-online.de>
2025-02-23 14:38:02 +0100

    feat(frontend): include user coordinates in be request

--- frontend/src/components/userInput.vue
+++ frontend/src/components/userInput.vue
@@ -5,31 +5,51 @@ const emit = defineEmits(['status-change', 'data-loaded']);
 
 const valid = ref(false);
 const genre = ref('');
-const location = ref('');
 const language = ref('');
 const errorMessage = ref('');
 
 const genreRules = [
     v => !!v || 'Genre is required',
 ];
-const locationRules = [
-    v => !!v || 'Location is required',
-];
 const languageRules = [
     v => !!v || 'Language is required',
 ];
 
+const getLocation = () => {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    resolve({
+                        lon: position.coords.longitude,
+                        lat: position.coords.latitude
+                    });
+                },
+                error => {
+                    console.log('Error:', error);
+                    reject(new Error('Failed to get location.'));
+                }
+            );
+        } else {
+            reject(new Error('Geolocation is not supported by this browser.'));
+        }
+    });
+};
+
 const submit = async () => {
     if (valid.value) {
         emit('status-change', 'loading');
-        const data = {
-            genre: genre.value,
-            location: location.value,
-            language: language.value,
-        };
-
         try {
-            const response = await fetch('http://localhost:8080/generateAd', {
+            const location = await getLocation();
+            console.log('Location:', location);
+            const data = {
+                genre: [genre.value],
+                lon: location.lon.toString(),
+                lat: location.lat.toString(),
+                language: language.value,
+            };
+
+            const response = await fetch('https://4argznoorj.execute-api.eu-central-1.amazonaws.com/dev/generated-ads', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -62,10 +82,6 @@ const submit = async () => {
             <v-col>
                 <v-row cols="12" md="4">
                     <v-text-field v-model="genre" :rules="genreRules" :counter="10" label="genre" required></v-text-field>
-                </v-row>
-
-                <v-row cols="12" md="4">
-                    <v-text-field v-model="location" :rules="locationRules" :counter="10" label="location" required></v-text-field>
                 </v-row>
 
                 <v-row cols="12" md="4">
