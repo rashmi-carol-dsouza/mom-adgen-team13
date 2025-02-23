acf9c7646c8490ca0c007bac661f57f93d87b804
Aaron D <aaron@dutschmann-online.de>
2025-02-23 14:05:39 +0100

    fix(frontend): fix routing issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,21 +3,29 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+import fs from 'fs';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'dist/index.html',
-          dest: 'dist/200.html'
+    {
+      name: 'generate-200-html',
+      apply: 'build',
+      closeBundle() {
+        const distPath = path.resolve(__dirname, 'dist');
+        const indexHtml = path.join(distPath, 'index.html');
+        const copyTo = path.join(distPath, '200.html');
+
+        if (fs.existsSync(indexHtml)) {
+          fs.copyFileSync(indexHtml, copyTo);
+          console.log('✅ 200.html generated');
         }
-      ]
-    })
+      }
+    }
   ],
   resolve: {
     alias: {
