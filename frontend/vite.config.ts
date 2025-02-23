d773aa50f60d3e459f6b1e10dc0a2c46509709a0
Aaron D <aaron@dutschmann-online.de>
2025-02-23 12:07:39 +0100

    feat(frontend): fix routing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: 'dist/200.html'
+        }
+      ]
+    })
   ],
   resolve: {
     alias: {
